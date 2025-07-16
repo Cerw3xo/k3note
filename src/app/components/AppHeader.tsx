004e23b61216eeb1e0a1155165b6b03f1ba93d02
Cerw3xo <149335179+Cerw3xo@users.noreply.github.com>
2025-07-16 20:09:55 +0200

    Iphone style notification badge

--- src/app/components/AppHeader.tsx
+++ src/app/components/AppHeader.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { FaRegBell } from "react-icons/fa";
 
 export default function AppHeader() {
-  const [notificationCount, setNotificationCount] = useState(3);
+  const [notificationCount, setNotificationCount] = useState(2);
 
   return (
     <header className="bg-white shadow-lg px-20 py-3 flex items-center justify-between">
@@ -19,21 +19,37 @@ export default function AppHeader() {
       <div>
         <nav>
           <ul className="flex gap-x-6 text-md font-medium">
-            <li> Dashboard</li>
-            <li> Nástenka</li>
-            <li> História</li>
+            <li>Dashboard</li>
+            <li>Nástenka</li>
+            <li>História</li>
           </ul>
         </nav>
       </div>
 
-      <div className="flex items-center gap-x-5">
-        <p>Status</p>
-        <FaRegBell className="w-4 h-4 text-k3-green" />
-        <p className="text-k3-red">{notificationCount}</p>
-      </div>
+      <div className="flex items-center gap-x-10">
+        <div className="relative inline-block cursor-pointer group">
+          <FaRegBell className="w-6 h-6 text-k3-blue rotate-12 group-hover:rotate-0 group-hover:scale-110 transition-all duration-200 " />
 
-      <div>
-        <p className="flex text-md font-medium">Matej</p>
+          {notificationCount > 0 && (
+            <span
+              className="bg-k3-red
+               text-white text-xs
+               rounded-full
+               flex items-center justify-center
+               px-1
+               absolute
+                top-3
+                -right-1
+                min-w-[10px]
+                min-h-[10px]
+                "
+            >
+              {" "}
+              {notificationCount > 99 ? "99+" : notificationCount}
+            </span>
+          )}
+        </div>
+        <p className="flex text-md font-medium ">Matej</p>
       </div>
     </header>
   );
